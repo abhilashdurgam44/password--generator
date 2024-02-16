b5e8e541bd789257962c4b074461450ae8ef7dee
abhilashdurgam44 <146201891+abhilashdurgam44@users.noreply.github.com>
2024-02-16 00:53:10 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-500">
+        <h1 className="text-center"> Password Generator</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
